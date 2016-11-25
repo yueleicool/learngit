9c074ecbf66b2fe53d526b66042ffa9cbc65eb9d
yue123 <yue_lei_cool@126.com>
2016-11-25 04:46:26 -0500

    1212

--- udp-client.py
+++ udp-client.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import socket  
-  
+       
 address = ('127.0.0.1', 31500)  
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
   
 while True:  
     msg = raw_input()  
-    if not msg:  
+    if not msg or msg=='quit':  
         break  
     s.sendto(msg, address)  
   
